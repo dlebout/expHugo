9b213dc56cfe81a608fa749ca29d6b3828fb9442
Lebout <dylan.lebout@gmail.com>
2018-04-27 09:47:10 +0200

    final exp design

--- model/expe.js
+++ model/expe.js
@@ -2,102 +2,48 @@
 
 var Expe = function (array) {
     this.array_expe = array;
-    console.log(array);
-    this.max_id_expe = array.length;
-    this.state = 0;
-    this.header = null;
-    this.description = null;
-    this.group_count = null;
-    this.dataSet = "no_dataset";
-    this.main_trial = null;
-    this.graph_size = null;
-    this.is_new_block = false;
-    this.trial = 0;
-    this.block = 0;
-    this.mainMotion = this.array_expe[this.state].mainMotion
-    //console.log(array)
+    this.max_trial = array.length;
+    this.index_reader = 0;
+
+    this.graph = "";
+    this.edges = "";
 }
 /****** PERMET DE PASSER AU TRIAL SUIVANT ********/
 Expe.prototype.set_new_state = function (state) {
-    this.state = state;
+    this.index_reader = state;
     this.set_infos();
 }
 
 Expe.prototype.set_infos = function () {
-  this.is_new_block = true;
-  this.block = this.array_expe[this.state].block;
-  this.trial = this.array_expe[this.state].trial;
-  this.practice = this.array_expe[this.state].practice;
-  this.graph_size = this.array_expe[this.state].graphSize.split('-')[0];
-  this.template = this.array_expe[this.state].graphSize.split('-')[1];
-  //this.graph_size = this.array_expe[this.state].graphSize.split('-')[0];
-  //this.template = this.array_expe[this.state].graphSize.split('-')[1];
-  //this.motion_variable = this.array_expe[this.state].motionVariable;
-  this.speed = this.array_expe[this.state].speed
-  this.speedMapping = this.array_expe[this.state].speedMapping
-  this.temporal = this.array_expe[this.state].temporal
-  this.temporalMapping = this.array_expe[this.state].temporalMapping
-  this.frequency = this.array_expe[this.state].frequency
-  this.frequencyMapping = this.array_expe[this.state].frequencyMapping
-  this.cross = this.array_expe[this.state].cross
-  this.crossMapping = this.array_expe[this.state].crossMapping
-  this.mainMotion = this.array_expe[this.state].mainMotion
-  this.color = this.array_expe[this.state].color
-  this.colorMapping = this.array_expe[this.state].colorMapping
-  this.size = this.array_expe[this.state].size
-  this.sizeMapping = this.array_expe[this.state].sizeMapping
-  this.shape = this.array_expe[this.state].shape
-  this.shapeMapping = this.array_expe[this.state].shapeMapping
-  this.edgeDistance = this.array_expe[this.state].edgeDistance
-  this.static_variables = []
+  this.trial = this.array_expe[this.index_reader].trial;
+  this.practice = this.array_expe[this.index_reader].practice;
 
-  this.dataSet = "LayoutFinal/"
-    + this.graph_size +"/"
-    + "Speed/" + this.speed + "/"
-    + "Temporal/" + this.temporal + "/"
-    + "Frequency/" + this.frequency + "/"
-    + "Cross/" + this.cross + "/"
-    + "Color/" + this.color + "/"
-    + "Size/" + this.size + "/"
-    + "Shape/" + this.shape + "/"
-    + "Template"+this.template + "/"
-  //  + "MappinMotion"+this.motionMapping + "/"
-    + "data_expe_temporal.json"//"data_fixed.json";//this.array_expe[this.state].dataSet;
-  this.edge = "LayoutFinal/"
-    + this.graph_size +"/"
-    + "Speed/" + this.speed + "/"
-    + "Temporal/" + this.temporal + "/"
-    + "Frequency/" + this.frequency + "/"
-    + "Cross/" + this.cross + "/"
-    + "Color/" + this.color + "/"
-    + "Size/" + this.size + "/"
-    + "Shape/" + this.shape + "/"
-    + "Template"+this.template + "/"
-    //+ "MappinMotion"+this.motionMapping + "/"
-    + "edges.json"//"data_fixed.json";//this.array_expe[this.state].dataSet;
-  this.edgeOption = [this.array_expe[this.state].edgeDistance,
-                    this.array_expe[this.state].speedMapping,
-                    this.array_expe[this.state].temporalMapping,
-                    this.array_expe[this.state].frequencyMapping,
-                    this.array_expe[this.state].colorMapping,
-                    this.array_expe[this.state].sizeMapping,
-                    this.array_expe[this.state].shapeMapping]
+  this.graph = this.array_expe[this.index_reader].graph;
+  this.distance = this.array_expe[this.index_reader].distance;
+  this.speed_reference = this.array_expe[this.index_reader].speed_reference;
+  this.speed_target = this.array_expe[this.index_reader].speed_target;
+  this.luminance_pair = this.array_expe[this.index_reader].luminance_pair;
+  this.assign_to = this.array_expe[this.index_reader].assign_to;
 
-  this.description = "TEXT";
+  this.dataSet = "data/speed_luminance_achromatic/graph.json"
+  this.edge = "data/speed_luminance_achromatic/edgesUS.json"
+
+  this.edgeOption = [
+    this.distance,
+    this.speed_reference,
+    this.speed_target,
+    this.luminance_pair,
+    this.assign_to
+  ]
 
   console.log(this.dataSet);
   console.log(this.edge);
   console.log(this.edgeOption);
-  // if (this.visualVariable == "TemporalDistribution") { this.header = "temporal distributions";}
-  // if (this.visualVariable == "PatternFrequency") { this.header = "frequencies";}
-  // if (this.visualVariable == "Speed") { this.header = "speeds";}
 
 }
 
 Expe.prototype.show_infos = function () {
-
   console.log(this.header,this.is_new_block, this.block ,this.trial,this.visualVariable ,this.dataSet,this.description);
 }
-
 
 module.exports = Expe;
